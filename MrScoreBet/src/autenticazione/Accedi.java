70f4519c3011eced8dcc7616ec0f7db9fa1161e7
VincRoffo <v.roffo@studenti.unina.it>
2018-11-20 17:26:56 +0100

    Bug inspiegabile.

--- MrScoreBet/src/autenticazione/Accedi.java
+++ MrScoreBet/src/autenticazione/Accedi.java
@@ -8,6 +8,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,6 +20,7 @@ import org.json.JSONObject;
 /**
  * Servlet implementation class Accedi
  */
+@WebServlet(urlPatterns={"/accedi"})
 public class Accedi extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -29,84 +31,91 @@ public class Accedi extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request,response);
-	}
-
 	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 1) L'utente ha già contattato Fb
 		// TO-DO: controllare se l'utente ha veramente fatto l'accesso, potrebbe averlo negato e quindi qui ho un errore
 		// vedere documentazione Facebook
 		String code = request.getParameter("code");
 	    String scope = request.getParameter("scope");
-	    
 	    // 2) Richiesta access_token a Fb fornendo il code
 	    URL oauth = new URL(" https://graph.facebook.com/v3.2/oauth/access_token?" + 
 	    		"client_id=2095469647430370" + 
-	    		"&redirect_uri=localhost:8080"+request.getContextPath()+"/app/user.jsp" + 
+	    		"&redirect_uri=http://localhost:8080"+request.getContextPath()+"/accedi"+ 
 	    		"&client_secret=d86e6c7a71084976e0d1747467dbd580" +
 	    		"&code="+code);
 	    		
         HttpURLConnection connection = (HttpURLConnection) oauth.openConnection();
         connection.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(
-                                new InputStreamReader(
-                                connection.getInputStream()));
+        
+        BufferedReader in;
+        if (connection.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+        	in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        	System.out.println(connection.getResponseCode());
+        } else {
+            in = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        }
         
         StringBuilder sb = new StringBuilder();
-        String line,token = null,tokentype,expires;
+        String line,token = null,tokentype;
+        Double expires;
         while ((line = in.readLine()) != null) {
             sb.append(line);
         }
         in.close();
-        
+        JSONObject json;
         try {
-			JSONObject json = new JSONObject(sb.toString());
+			json = new JSONObject(sb.toString());
 			token = json.getString("access_token");
+			System.out.println(token);
 	        tokentype = json.getString("token_type");
-	        expires = json.getString("expires_in");
+	        System.out.println(tokentype);
+	        expires = json.getDouble("expires_in");
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
         
         // 3) Ispezione dell'access_token
         // TO-DO: se non è valido (scaduto o revocato, bisogna settare una variabile di sessione e reindirizzare l'utente a index.jsp)
-        URL oinspect = new URL(" https://graph.facebook.com/debug_token?" + 
+       
+        URL oinspect = new URL("https://graph.facebook.com/debug_token?" + 
         		"input_token="+ token + 
         		"&access_token=62d1ca33988d65db393e1bda9f06c58b");
 	    		
         HttpURLConnection connection2 = (HttpURLConnection) oinspect.openConnection();
         connection2.setRequestMethod("GET");
-        BufferedReader in2 = new BufferedReader(
-                                new InputStreamReader(
-                                connection2.getInputStream()));
+        
+        BufferedReader in2;
+        if (connection2.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+        	in2 = new BufferedReader(new InputStreamReader(connection2.getInputStream()));
+        	System.out.println("GOT THE INFOS");
+        } else {
+            in2 = new BufferedReader(new InputStreamReader(connection2.getErrorStream()));
+            System.out.println("400");
+        }
         
         StringBuilder sb2 = new StringBuilder();
         while ((line = in2.readLine()) != null) {
             sb2.append(line);
+            System.out.println(line);
         }
-        in.close();
+        in2.close();
         
-        String validity="false", userid="0000";
+        String userid="0000";
+        Boolean validity=false;
         try {
-			JSONObject json = new JSONObject(sb2.toString());
-			validity = json.getString("is_valid");
-	        userid = json.getString("user_id");
+			JSONObject json2 = new JSONObject(sb2.toString());
+			validity = json2.getBoolean("is_valid");
+	        userid = json2.getString("user_id");
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
         
-        if(validity.equals("true")) {
+        if(validity) {
         	HttpSession sessione = request.getSession();
         	sessione.setAttribute("utente", userid );
         	request.getRequestDispatcher( "/app/user.jsp" ).forward(request,response);
