c329241f8e22d1de9538100be5e2dc09cafd3630
kraken1105 <piccirillo.pierpaolo95@gmail.com>
2018-11-27 13:41:09 +0100

    scheletro database

---
+++ MrScoreBet/src/utils/ServerDatabase.java
@@ -0,0 +1,38 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class ServerDatabase {
+	
+	public static void connect() {
+		Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/MrScoreBet/MrScoreBet.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("[DEBUG] Connessione to SQLite stabilita.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+	
+	
+}
+    
